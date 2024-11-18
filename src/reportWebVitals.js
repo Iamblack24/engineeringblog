4b6db46b35c678787e6a438d528353c88e16129e
Iamblack24 <johnicarus2020@students.tukenya.ac.ke>
2024-11-18 19:41:05 +0300

    ggotags

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,5 @@
-const reportWebVitals = onPerfEntry => {
+/* global gtag */
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
@@ -10,4 +11,22 @@ const reportWebVitals = onPerfEntry => {
   }
 };
 
-export default reportWebVitals;
+const sendToAnalytics = ({ name, delta, id }) => {
+  gtag('event', name, {
+    event_category: 'Web Vitals',
+    event_label: id,
+    value: Math.round(name === 'CLS' ? delta * 1000 : delta), // values must be integers
+    non_interaction: true, // avoids affecting bounce rate
+  });
+};
+
+const report = (onPerfEntry) => {
+  reportWebVitals((metric) => {
+    sendToAnalytics(metric);
+    if (onPerfEntry) {
+      onPerfEntry(metric);
+    }
+  });
+};
+
+export default report;
