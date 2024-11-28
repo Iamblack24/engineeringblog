77cf7ccfa2a46d2f7d6bc1a6da8c245df8bfae69
Iamblack24 <johnicarus2020@students.tukenya.ac.ke>
2024-11-28 17:55:21 +0300

    moneta

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Caching Logic
-const CACHE_NAME = 'enghub-cache-v12';
+const CACHE_NAME = 'enghub-cache-v13';
 const urlsToCache = [
   '/',
   '/index.html',
