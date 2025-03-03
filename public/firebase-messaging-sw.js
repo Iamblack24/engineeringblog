263fd889742d6631b88d7648fe38a9ce84047ef1
Iamblack24 <johnicarus2020@students.tukenya.ac.ke>
2025-03-03 23:35:33 +0300

    Resolved mergegf

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 // Caching Logic
-const CACHE_NAME = 'enghub-cache-v40';
+const CACHE_NAME = 'enghub-cache-v41';
 const urlsToCache = [
   '/',
   '/index.html',
